a9f3d752c750245d4089a9a73767081c3dcb81e7
Ozonlagret <tim.drejhede@gmail.com>
2025-10-01 15:04:16 +0200

    Added various properties to entities

--- Domain/Entities/Activity.cs
+++ Domain/Entities/Activity.cs
@@ -8,5 +8,8 @@ namespace Domain.Entities
 {
     public class Activity
     {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Description { get; set; } = null;
     }
 }
--- Domain/Entities/Location.cs
+++ Domain/Entities/Location.cs
@@ -8,5 +8,10 @@ namespace Domain.Entities
 {
     public class Location
     {
+        public int Id { get; set; }
+        public string? City { get; set; }
+        public string? Address { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; } 
     }
 }
