fb70165ac05a2cc0acec49ede68d82c3e447f1cc
Ozonlagret <tim.drejhede@gmail.com>
2025-10-07 15:11:41 +0200

    Added IActivityRepository methods

---
+++ Infrastructure/Repositories/IActivityRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Domain.Entities;
+
+namespace Infrastructure.Repositories
+{
+    public interface IActivityRepository
+    {
+        Task<Activity?> GetByIdAsync(int id);
+        Task<Activity?> GetByIdWithDetailsAsync(int id); 
+        Task<IEnumerable<Activity>> GetAllAsync();
+        Task<IEnumerable<Activity>> GetActiveAsync();
+        Task<IEnumerable<Activity>> GetByCategoryAsync(int categoryId);
+        Task<IEnumerable<Activity>> SearchAsync(string? category, bool? isIndoor);
+        Task AddAsync(Activity activity);
+        Task UpdateAsync(Activity activity);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
