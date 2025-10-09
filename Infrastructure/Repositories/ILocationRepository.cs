557b78ab5be9dc3b0b35fe6a5e41f0b08c089dd6
Ozonlagret <tim.drejhede@gmail.com>
2025-10-09 13:51:46 +0200

    Added various repository methods to LocationRepository:
    - GetByIdAsync(int id)
    - GetByIdWithSessionsAsync(int id)
    - GetAllAsync()
    - GetActiveAsync()
    - GetByTypeAsync()
    - AddAsync()
    - Update()
    - DeleteAsync()
    - ExistsAsync()

---
+++ Infrastructure/Repositories/ILocationRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface ILocationRepository
+    {
+        Task<Location?> GetByIdAsync(int id);
+        Task<Location?> GetByIdWithSessionsAsync(int id);
+        Task<IEnumerable<Location>> GetAllAsync();
+        Task<IEnumerable<Location>> GetActiveAsync();
+        Task<IEnumerable<Location>> GetByTypeAsync(bool isIndoor);
+        Task AddAsync(Location location);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+    }
+}
--- Infrastructure/Repositories/LocationRepository.cs
+++ Infrastructure/Repositories/LocationRepository.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using Domain.Entities;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +9,78 @@ using System.Threading.Tasks;
 
 namespace Infrastructure.Repositories
 {
-    internal class LocationRepository
+    public class LocationRepository : ILocationRepository
     {
+        private readonly ActiviGoDbContext _context;
+
+        public LocationRepository(ActiviGoDbContext context)
+        {
+            _context = context;
+        }
+
+        // CHANGES ARE TO BE SAVED IN SERVICE LAYER
+
+        public async Task<Location?> GetByIdAsync(int id)
+        {
+            return await _context.Locations.FindAsync(id);
+        }
+
+        public async Task<Location?> GetByIdWithSessionsAsync(int id)
+        {
+            return await _context.Locations
+                .Include(l => l.Sessions)
+                .FirstOrDefaultAsync();
+        }
+
+        public async Task<IEnumerable<Location>> GetAllAsync()
+        {
+            return await _context.Locations
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Location>> GetActiveAsync()
+        {
+            return await _context.Locations
+                .Where(l => l.IsActive == true)
+                .ToListAsync();
+        }
+
+        public async Task<IEnumerable<Location>> GetByTypeAsync(bool isIndoor)
+        {
+            if (isIndoor == true)
+            {
+                return await _context.Locations
+                    .Where(l => l.IsIndoor)
+                    .ToListAsync();
+            }
+            else
+            {
+                return await _context.Locations
+                    .Where(l => !l.IsIndoor)
+                    .ToListAsync();
+            }
+        }
+
+        public async Task AddAsync(Location location)
+        {
+            await _context.Locations.AddAsync(location);
+        }
+
+        public void Update(Location location)
+        {
+            _context.Locations.Update(location);
+        }
+
+        public async Task DeleteAsync(int id)
+        {
+            var location = await _context.Locations.FindAsync(id);
+            if (location != null)
+                _context.Locations.Remove(location);
+        }
+
+        public async Task<bool> ExistsAsync(int id)
+        {
+            return await _context.Locations.AnyAsync(a => a.Id == id);
+        }
     }
 }
