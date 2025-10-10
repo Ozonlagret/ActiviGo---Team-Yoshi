6d2375e2903de555647060ea52be7ab48d6b7d12
Ozonlagret <tim.drejhede@gmail.com>
2025-10-10 14:33:33 +0200

    Methods added for ICategoryRepository + CategoryRepository

---
+++ Infrastructure/Repositories/ICategoryRepository.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure.Repositories
+{
+    public interface ICategoryRepository
+    {
+        Task<Category?> GetByIdAsync(int id);
+        Task<Category?> GetByIdWithActivitiesAsync(int id);
+        Task<IEnumerable<Category>> GetAllAsync();
+        Task AddAsync(Category category);
+        Task UpdateAsync(Category category);
+        Task DeleteAsync(int id);
+        Task<bool> ExistsAsync(int id);
+        Task<bool> HasActivitiesAsync(int id);
+    }
+}
