72202b6d4f398501c83bc9299a5fe8d1662d19d8
Ozonlagret <tim.drejhede@gmail.com>
2025-10-13 15:47:08 +0200

    Revert "gitignore on dependencyinjection because it kept being annoying"
    This reverts commit 03b17d20e4f7a26d8a258f9d60a111262e21194c.

---
+++ Infrastructure/DependencyInjection.cs
@@ -0,0 +1,44 @@
+using Application.Interfaces;
+using Application.Options;
+using Domain.Entities;
+using Infrastructure.Auth;
+using Infrastructure.Data;
+using Infrastructure.Repositories;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure
+{
+    public static class DependencyInjection
+    {
+        // Anropa denna från Program.cs: services.AddInfrastructure(builder.Configuration)
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services, IConfiguration config)
+        {
+            services.AddDbContext<ActiviGoDbContext>(opt =>
+            {
+                 opt.UseNpgsql(config.GetConnectionString("DefaultConnection"));
+            });
+
+            // Identity (int-nycklar => IdentityRole<int>)
+            services.AddIdentityCore<ApplicationUser>(o =>
+            {
+                o.User.RequireUniqueEmail = true;
+                // o.Password.RequiredLength = 6; osv (valfritt)
+            })
+            .AddRoles<IdentityRole<int>>()
+            .AddEntityFrameworkStores<ActiviGoDbContext>();
+
+            // JWT options + token generator
+            services.Configure<JwtOptions>(config.GetSection("Jwt"));
+            services.AddScoped<IJwtTokenGenerator, JwtTokenGenerator>();
+
+            return services;
+        }
+
+        // Behåll en parameterlös overload om andra projekt redan kallar den
+        public static IServiceCollection AddInfrastructure(this IServiceCollection services)
+            => throw new InvalidOperationException("Call AddInfrastructure(IConfiguration) instead.");
+    }
+}
