e60b658bd10412ab52f04ed0efc30e244f8dfc41
Ozonlagret <tim.drejhede@gmail.com>
2025-10-25 13:10:06 +0200

    Add request dto for activitySession filter endpoint to Application/DTOs/Requests

---
+++ Application/DTOs/Requests/FilterSessionsRequest.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.DTOs.Requests
+{
+    public class FilterSessionsRequest
+    {
+        public DateTime? StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public int? CategoryId { get; set; }
+        public bool? IsIndoor { get; set; }
+        public int? LocationId { get; set; }
+    }
+}
