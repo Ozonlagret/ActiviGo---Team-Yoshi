f242bd8f0956697744f267502335466179a93980
Ozonlagret <tim.drejhede@gmail.com>
2025-10-25 14:24:36 +0200

    Add ActivitySessionController and FilterAvailableSession method in said controller

---
+++ ActiviGo/Controllers/ActivitySessionsController.cs
@@ -0,0 +1,24 @@
+﻿using Application.Interfaces.Service;
+using Application.DTOs.Requests;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ActiviGo.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ActivitySessionsController : ControllerBase
+    {
+        private readonly IActivitySessionService _activitySessionService;
+        public ActivitySessionsController(IActivitySessionService activitySessionService)
+            => _activitySessionService = activitySessionService;
+
+        [HttpPost("filter")]
+        public async Task<IActionResult> FilterAvailableSessions([FromBody] FilterSessionsRequest request, CancellationToken ct)
+        {
+            var sessions = await _activitySessionService.FilterAvailableSessionsAsync(request, ct);
+            return Ok(sessions);
+        }
+
+    }
+}
